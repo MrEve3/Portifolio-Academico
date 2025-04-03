7f3692350c6b78799586baa867514c064e78b0b8
Everton <evertoncunhaoliveira14@gmail.com>
2025-04-02 21:23:31 -0300

    Aula 02/04/2025

---
+++ aula 02042025/pgm02.cpp
@@ -0,0 +1,11 @@
+//pergunte ao usuario a idade e mostre na tela
+#include<stdio.h> //biblioteca de entrade/saida
+#include<stdlib.h> //
+#include<locale.h>
+int main(){
+	int idade;
+	setlocale(LC_ALL,"portuguese");
+	printf("Qual é a sua idade: ");
+	scanf("%d",&idade);
+	printf("Sua idade é %d ",idade);	
+}
